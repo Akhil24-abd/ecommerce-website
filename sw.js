58262c77223cd35d3615bc04ab34c392d320ed20
Akhil24-abd <akghnekar2020@gmail.com>
2022-04-08 00:56:07 +0530

    m

--- sw.js
+++ sw.js
@@ -63,6 +63,7 @@ self.addEventListener('fetch', (event) => {
         }
 
         // No cache match, we attempt to fetch it from the network
+        return fetch(event.request);
       }
     )
   );
